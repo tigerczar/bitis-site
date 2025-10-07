1e5bac94f7599e96ff7fb1bbe2808448001d6786
Jed Pham <haopv7@gmail.com>
2025-10-07 23:47:59 +0700

    upd test case

---
+++ tests/components/StoreLocator.spec.ts
@@ -0,0 +1,15 @@
+import { describe, it, expect } from 'vitest'
+import { mount } from '@vue/test-utils'
+import StoreLocator from '../../app/components/StoreLocator.vue'
+
+describe('Show StoreLocator', () => {
+  it('render component thành công', () => {
+    const wrapper = mount(StoreLocator)
+    expect(wrapper.find('h2').text()).toContain('HỆ THỐNG CỬA HÀNG')
+  })
+  
+  it('Have dropdown filter province', () => {
+    const wrapper = mount(StoreLocator)
+    expect(wrapper.find('select').exists()).toBe(true)
+  })
+})
